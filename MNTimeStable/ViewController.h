d1b17386b56e601d3012da57aed78376f7bf6120
Mrunalini Nemade <mrunalininemade.mn@gmail.com>
2016-09-28 17:46:30 +0530

    Initial Commit

--- MNTimeStable/ViewController.h
+++ MNTimeStable/ViewController.h
@@ -8,7 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITableViewDelegate,UITableViewDataSource>
+{
+    NSMutableArray *allValues;
+}
+
+@property (strong, nonatomic) IBOutlet UITextField *numberField;
+
+@property (strong, nonatomic) IBOutlet UITextField *limitField;
+@property (strong, nonatomic) IBOutlet UIButton *calculateButton;
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+- (IBAction)calculateButton:(id)sender;
+
 
 
 @end
